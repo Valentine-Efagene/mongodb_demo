a543c0a3061c5627692e6ad882d1a2798ec6f672
Valentine-Efagene <efagenevalentine@gmail.com>
2021-12-04 19:50:26 +0100

    First upload

---
+++ server/routes/record.js
@@ -0,0 +1,88 @@
+const express = require('express')
+
+// recordRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /listings.
+const recordRoutes = express.Router()
+
+// This will help us connect to the database
+const dbo = require('../db/conn')
+
+recordRoutes.route('/listings').get(async (_, res) => {
+  const dbConnect = dbo.getDb()
+
+  dbConnect
+    .collection('listingsAndReviews')
+    .find({})
+    .limit(50)
+    .toArray((err, result) => {
+      if (err) {
+        res.status(400).send('Error fetching listings!')
+      } else {
+        res.json(result)
+      }
+    })
+})
+
+// This section will help you create a new document.
+recordRoutes.route('/listings/recordSwipe').post((req, res) => {
+  const dbConnect = dbo.getDb()
+  const matchDocument = {
+    listing_id: req.body.id,
+    last_modified: new Date(),
+    session_id: req.body.session_id,
+    direction: req.body.direction,
+  }
+
+  dbConnect.collection('matches').insertOne(matchDocument, (err, result) => {
+    if (err) {
+      res.status(400).send('Error inserting matches!')
+    } else {
+      console.log(`Added a new match with id ${result.insertedId}`)
+      res.status(204).send()
+    }
+  })
+})
+
+// This section will help you update a document by id.
+recordRoutes.route('/listings/updateLike').post((req, res) => {
+  const dbConnect = dbo.getDb()
+  const listingQuery = { _id: req.body.id }
+  const updates = {
+    $inc: {
+      likes: 1,
+    },
+  }
+
+  dbConnect
+    .collection('listingsAndReviews')
+    .updateOne(listingQuery, updates, (err, _result) => {
+      if (err) {
+        res
+          .status(400)
+          .send(`Error updating likes on listing with id ${listingQuery._id}!`)
+      } else {
+        console.log('1 document updated')
+      }
+    })
+})
+
+// This section will help you delete a record.
+recordRoutes.route('/listings/delete/:id').delete((req, res) => {
+  const dbConnect = dbo.getDb()
+  const listingQuery = { listing_id: req.body.id }
+
+  dbConnect
+    .collection('listingsAndReviews')
+    .deleteOne(listingQuery, (err, _result) => {
+      if (err) {
+        res
+          .status(400)
+          .send(`Error deleting listing with id ${listingQuery.listing_id}!`)
+      } else {
+        console.log('1 document deleted')
+      }
+    })
+})
+
+module.exports = recordRoutes
